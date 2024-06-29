d9d97d22027e29cc48faea467483b8a4d2ecff62
alannetwork <alanestrada@cloudmex.io>
2024-06-28 23:07:29 -0700

    new sc

--- RegenerativeStakingSC/regenerativestaking.sol
+++ RegenerativeStakingSC/regenerativestaking.sol
@@ -1,25 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IUniswapV2Router {
-    function swapExactETHForTokens(
-        uint amountOutMin,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external payable returns (uint[] memory amounts);
-
-    function WETH() external pure returns (address);
-}
-
 interface IMPETH {
-    function stake() external payable;
-    function unstake(uint256 amount) external;
     function transfer(address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
 contract RegenerativeStaking {
-    address public immutable uniswapRouter;
     address public immutable mpETH;
     address public immutable donationWallet;
 
@@ -34,35 +22,23 @@ contract RegenerativeStaking {
     uint256 public constant LOCK_PERIOD = 365 days;
     uint256 public constant DONATION_PERCENT = 4;
 
-    constructor(address _uniswapRouter, address _mpETH, address _donationWallet) {
-        uniswapRouter = _uniswapRouter;
+    constructor(address _mpETH, address _donationWallet) {
         mpETH = _mpETH;
         donationWallet = _donationWallet;
     }
 
-    function donateAndStake() public payable {
-        require(msg.value > 0, "Must send ETH to donate and stake");
+    function donateAndStake(uint256 amount) public {
+        require(amount > 0, "Must send mpETH to donate and stake");
 
-        // Swap ETH to mpETH
-        address[] memory path = new address[](2);
-        path[0] = IUniswapV2Router(uniswapRouter).WETH();
-        path[1] = mpETH;
-
-        uint[] memory amounts = IUniswapV2Router(uniswapRouter).swapExactETHForTokens{value: msg.value}(
-            0,
-            path,
-            address(this),
-            block.timestamp + 600
-        );
-
-        uint256 mpEthAmount = amounts[amounts.length - 1];
+        // Transfer mpETH from the user to the contract
+        require(IMPETH(mpETH).transferFrom(msg.sender, address(this), amount), "Transfer failed");
 
         // Calculate donation amount and lock amount
-        uint256 donationAmount = (mpEthAmount * DONATION_PERCENT) / 100;
-        uint256 lockAmount = mpEthAmount - donationAmount;
+        uint256 donationAmount = (amount * DONATION_PERCENT) / 100;
+        uint256 lockAmount = amount - donationAmount;
 
         // Transfer donation amount to donation wallet
-        IMPETH(mpETH).transfer(donationWallet, donationAmount);
+        require(IMPETH(mpETH).transfer(donationWallet, donationAmount), "Transfer failed");
 
         // Record the staker's locked amount and lock time
         stakes[msg.sender].push(Stake({
@@ -81,33 +57,10 @@ contract RegenerativeStaking {
         stake.claimed = true;
 
         // Transfer the locked amount back to the user
-        IMPETH(mpETH).transfer(msg.sender, stake.amount);
+        require(IMPETH(mpETH).transfer(msg.sender, stake.amount), "Transfer failed");
     }
 
-    function claimInETH(uint256 stakeIndex) external {
-        require(stakeIndex < stakes[msg.sender].length, "Invalid stake index");
-        Stake storage stake = stakes[msg.sender][stakeIndex];
-        require(block.timestamp >= stake.lockTime, "Stake is still locked");
-        require(!stake.claimed, "Stake already claimed");
-
-        stake.claimed = true;
-
-        // Unstake and swap mpETH to ETH
-        IMPETH(mpETH).unstake(stake.amount);
-
-        address[] memory path = new address[](2);
-        path[0] = mpETH;
-        path[1] = IUniswapV2Router(uniswapRouter).WETH();
-
-        IUniswapV2Router(uniswapRouter).swapExactETHForTokens(
-            0,
-            path,
-            msg.sender,
-            block.timestamp + 600
-        );
-    }
-
-    receive() external payable {
-        donateAndStake();
+    function getStakes(address user) external view returns (Stake[] memory) {
+        return stakes[user];
     }
 }
