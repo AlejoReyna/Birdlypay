e10e2d658c01158811011c5189791799ba43d387
AlejoReyna <tunnel.ovbzc@aleeas.com>
2024-07-01 01:13:56 -0600

    Modified Landing Page

--- Birdlypay/app/landingPage/page.tsx
+++ Birdlypay/app/landingPage/page.tsx
@@ -22,7 +22,7 @@ export default function landingPage() {
                 
                
                 <div className="flex flex-col p-2 md:p-0  font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <button onClick={handleLogin}> <span className="text-xs"> Launch app </span> </button>   
+                    <button onClick={handleLogin}>  Launch app  </button>   
                 </div >
            
             </div>
@@ -34,33 +34,9 @@ export default function landingPage() {
 
         </div>
 
-        <div className="container-fluid min-h-screen page-two ">
-            <div className="flex justify-center">
-            <Image src={whiteLogo} alt="Just a white logo" className="birdLogo mt-3"/>
-            </div>
-            
-            <div className="flex justify-center text-center">
-                <p className='text-white m-6'> Discover the most practical way to use your crypto, organize, set goals, access incredible promotions and more benefits. </p>
-            </div>
+        
 
-            <div className="flex justify-center">
-                <Image src={celphone} alt='Cellphone image'/>
-            </div>
-        </div>
-
-        <div className="container-fluid min-h-screen page-three ">
-            <div className="flex justify-center">
-            <Image src={whiteLogo} alt="Just a white logo" className="birdLogo mt-3"/>
-            </div>
-            
-            <div className="flex justify-center text-center">
-                <p className='text-white m-6'> Discover the most practical way to use your crypto, organize, set goals, access incredible promotions and more benefits. </p>
-            </div>
-
-            <div className="flex justify-center">
-                <Image src={celphone} alt='Cellphone image'/>
-            </div>
-        </div>
+       
   
     </>
     );
